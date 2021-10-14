de67aa8621f477d57d24781d45f0eb5b2fd7f983
tilekbergen14 <tilekbergen.third@gmail.com>
2021-10-14 17:13:42 +0600

    initial commit

--- src/Context.js
+++ src/Context.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import books from "./books";
+import booksData from "./books";
 
 const BookStoreContext = React.createContext();
 
 export const ContextProvider = (props) => {
   const [bookIds, setBookIds] = useState([]);
+  const [books, setBooks] = useState(booksData);
   const addToCard = (id) => {
     if (bookIds.includes(id)) {
       setBookIds(bookIds.filter((book) => book !== id));
@@ -14,8 +15,16 @@ export const ContextProvider = (props) => {
   };
   const children = props.children;
   const pathname = props.location && props.location.pathname;
+  const onSearch = (e) => {
+    const searchResults = booksData.filter((book) =>
+      book.title.toLowerCase().includes(e)
+    );
+    setBooks(searchResults);
+  };
   return (
-    <BookStoreContext.Provider value={{ books, bookIds, pathname, addToCard }}>
+    <BookStoreContext.Provider
+      value={{ books, bookIds, pathname, addToCard, onSearch }}
+    >
       {children}
     </BookStoreContext.Provider>
   );
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import Card from "../components/Card";
-import books from "../books";
+import Context from "../Context";
+import SearchBar from "../components/SearchBar";
+
 export default function Homepage() {
+  const { books } = useContext(Context);
   return (
-    <>
-      {books.map((book) => {
-        return <Card key={book.id} book={book} />;
-      })}
-    </>
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <SearchBar />
+      <div style={{ width: "100%", display: "flex", flexWrap: "wrap" }}>
+        {books.map((book) => {
+          return <Card key={book.id} book={book} />;
+        })}
+      </div>
+    </div>
   );
 }
